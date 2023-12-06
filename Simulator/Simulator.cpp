c4552806b1bd45d551a102dcd1b98bdc9eccf1ea
Mubarak Andrabi <andrabimubi@gmail.com>
2023-12-06 17:55:46 +0500

    Good Bounce

--- Simulator/Simulator.cpp
+++ Simulator/Simulator.cpp
@@ -8,11 +8,13 @@ int main() {
     int WIDTH = 800;
     int HEIGHT = 800;
     float EARTH_GRAVITY = 9.81 / 8; 
-    float BOUNCE_FACTOR_RUBBER = 0.7;
+    float BOUNCE_FACTOR_RUBBER = 1;
     float time = 0.0;
     int i = 0;
+    int k = 0;
     float up = 0;
     bool ground = false;
+    int check = 0;
 
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Bouncing Ball");
     window.setFramerateLimit(60);
@@ -56,24 +58,29 @@ int main() {
         
         up = up - EARTH_GRAVITY;
         
-        if (ball.getPosition().x >= WIDTH - ball.getRadius() * 2 || ball.getPosition().x <= 0) {
-            
-            ballSpeed.x = -ballSpeed.x * BOUNCE_FACTOR_RUBBER;
-        }
         
-        if (ball.getPosition().y >= HEIGHT - ball.getRadius()) {
-            
-            time = (ballSpeed.y * 2.83 ) / (EARTH_GRAVITY * 8);
-            std::cout << "Ball speed = " << ballSpeed.y *2.83 <<  " ; time = " << time << std::endl;
-            ballSpeed.y = -ballSpeed.y * BOUNCE_FACTOR_RUBBER;
-            if (ballSpeed.y > 0)
+        
+        if (ball.getPosition().y >= HEIGHT - (ball.getRadius()*2)) {
+            if (ground != true) {
+                if (i - k - 1 == 0)
+                    continue;
+                time = (ballSpeed.y * 2.83) / (EARTH_GRAVITY * 8);
+                check = ballSpeed.y;
+                std::cout << "Ball speed = " << ballSpeed.y * 2.83 << " ; time = " << time << std::endl;
+                ballSpeed.y = -ballSpeed.y * BOUNCE_FACTOR_RUBBER;
+                ground = false;
+                std::cout << "Bounce Speed  " << ballSpeed.y << std::endl;
+                k = i;
+            }
+            if (ballSpeed.y > -3)
             {
                 ground = true;
-                ball.setPosition(ball.getPosition().x,ball.getPosition().y - ball.getRadius());
+                ball.setPosition(ball.getPosition().x,HEIGHT - ball.getRadius()*2);
+                
             
             }
         }
-        
+        std::cout << "Iteration = " << i << " ; Ball speed = " << ballSpeed.y << std::endl;
         i++;
         window.clear();
         window.draw(ball);
