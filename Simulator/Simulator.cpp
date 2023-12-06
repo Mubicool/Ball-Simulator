be542273c5ccc889e33ef96ef419d7ef2aa7e7f3
Mubarak Andrabi <andrabimubi@gmail.com>
2023-12-06 19:58:26 +0500

    Added Top Bounce

--- Simulator/Simulator.cpp
+++ Simulator/Simulator.cpp
@@ -8,12 +8,16 @@ int main() {
     int WIDTH = 800;
     int HEIGHT = 800;
     float EARTH_GRAVITY = 9.81 / 8; 
-    float BOUNCE_FACTOR_RUBBER = 1;
+    float bouce_fac =0.7;
     float time = 0.0;
     int i = 0;
     int k = 0;
+    int j = 0;
+    int l = 0;
     float up = 0;
     bool ground = false;
+    bool top = false;
+    
     int check = 0;
 
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Bouncing Ball");
@@ -21,7 +25,7 @@ int main() {
 
     sf::CircleShape ball(20);
     ball.setFillColor(sf::Color::Red);
-    ball.setPosition(WIDTH / 2 - ball.getRadius(), -100);
+    ball.setPosition(WIDTH / 2 - ball.getRadius(), HEIGHT / 2 - ball.getRadius());
 
     sf::Vector2f ballSpeed(0, 0);
 
@@ -31,51 +35,98 @@ int main() {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        if (ballSpeed.y < 30) {
+        if (ballSpeed.y > 100 || ballSpeed.y < -100) {
+            /*if (ballSpeed.y > 100)*/
+            ballSpeed.y = 100;
+           /* if (ballSpeed.y < -100)
+                ballSpeed.y = -100;*/
+
+
+        }
+        if (ballSpeed.y < 30 && ground != true) {
             
             ballSpeed.y += EARTH_GRAVITY;
             
         }
-        
+        ballSpeed.x = 0;
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
         {
-            
+            if (top == true)
+                continue;
             ground = false;
+            
                 up = 20;
-                
+                l = i;
                 
             
         }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+        {
+
+            ballSpeed.x = 5;
+            ball.setPosition(ball.getPosition().x + ballSpeed.x, ball.getPosition().y);
+
+
+
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+        {
+
+            ballSpeed.x = -5;
+            ball.setPosition(ball.getPosition().x + ballSpeed.x, ball.getPosition().y);
+
+
+
+        }
         if (up > 0) {
-            ballSpeed.y = 0;
-            ball.setPosition(ball.getPosition().x, ball.getPosition().y - up);
+            ballSpeed.y = up;
+            ball.setPosition(ball.getPosition().x, ball.getPosition().y - ballSpeed.y);
+           // std::cout << "up speed = " << up << std::endl;
         }
 
         else {
             if(ground != true)
             ball.setPosition(ball.getPosition() + ballSpeed);
+            top = false;
         }
         
         up = up - EARTH_GRAVITY;
         
-        
-        
+        if (ball.getPosition().x >= WIDTH - ball.getRadius() * 2 || ball.getPosition().x <= 0) {
+
+            ballSpeed.x = -ballSpeed.x * bouce_fac;
+        }
+        if (ball.getPosition().y <= 0)
+        {
+            if (i - j == 1)
+                continue;
+            top = true;
+            
+            ballSpeed.y = up + ballSpeed.y;
+            up = 0;
+            ballSpeed.y = ballSpeed.y * 2 * bouce_fac ;
+            int j = i;
+            ball.setPosition(ball.getPosition().x,0);
+            //std::cout << "top !!" << std::endl;
+         
+        }
         if (ball.getPosition().y >= HEIGHT - (ball.getRadius()*2)) {
             if (ground != true) {
                 if (i - k - 1 == 0)
                     continue;
                 time = (ballSpeed.y * 2.83) / (EARTH_GRAVITY * 8);
                 check = ballSpeed.y;
-                std::cout << "Ball speed = " << ballSpeed.y * 2.83 << " ; time = " << time << std::endl;
-                ballSpeed.y = -ballSpeed.y * BOUNCE_FACTOR_RUBBER;
+                //std::cout << "Ball speed = " << ballSpeed.y * 2.83 << " ; time = " << time << std::endl;
+                ballSpeed.y = -ballSpeed.y * bouce_fac;
                 ground = false;
-                std::cout << "Bounce Speed  " << ballSpeed.y << std::endl;
+                //std::cout << "Bounce Speed  " << ballSpeed.y << std::endl;
                 k = i;
             }
             if (ballSpeed.y > -3)
             {
                 ground = true;
                 ball.setPosition(ball.getPosition().x,HEIGHT - ball.getRadius()*2);
+                ballSpeed.y = 0;
                 
             
             }
